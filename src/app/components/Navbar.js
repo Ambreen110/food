ae081f87faced4f56299568d76e7c6b398c4ddf1
ambreen110 <tassawerz@gmail.com>
2024-10-08 11:58:55 +0500

    Navbar small change

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -42,7 +42,7 @@ const Navbar = () => {
   };
 
   return (
-    <nav className={`fixed top-0 left-0 right-0 z-50 p-4 transition-all duration-300 ${isScrolled ? ' shadow-md' : 'bg-transparent'}`}>
+    <nav className={`fixed top-0 left-0 right-0 z-50 p-4 transition-all duration-300 $`}>
       <div className="container mx-auto flex justify-between items-center">
         {/* Logo/Image Section */}
         <div className="flex items-center">
